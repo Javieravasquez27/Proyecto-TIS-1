2bc31a1865bb1e6a977ba5750f3808307af8d864
Neovix33 <vlillo@ing.ucsc.cl>
2024-10-07 12:28:09 -0300

    Se carga mantenedores region y ciudad

---
+++ mantenedor_ciudad/ingresar_ciudad.php
@@ -0,0 +1,12 @@
+<?php
+    require('conexion.php');
+
+    $ciudad_registro= $_POST["ciudad_e"];
+    $id_r = $_POST["id_e"];
+
+    $consulta = "INSERT INTO ciudad (nombre_ciudad, id_region) VALUES('$ciudad_registro', '$id_r')";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    header('Location: index_ciudad.php');
+
+?>
