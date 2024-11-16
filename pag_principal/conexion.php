6aa69ccab1086cdf058b42e8f33cf65cc37e437a
Perro-Wacho <111267236+Perro-Wacho@users.noreply.github.com>
2024-11-16 00:23:58 -0300

    Delete pag_principal directory
    Signed-off-by: Perro-Wacho <111267236+Perro-Wacho@users.noreply.github.com>

--- pag_principal/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conexion = mysqli_connect("localhost", "root", "", "kindomjobs");
-
-    if (mysqli_connect_error())
-    {
-        die("Conexion Fallida con la bd: " . mysqli_connect_error());
-    }
-?>
