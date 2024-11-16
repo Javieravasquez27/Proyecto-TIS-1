dd03da4418e5ac21780b823292e655b2eda6f291
Perro-Wacho <111267236+Perro-Wacho@users.noreply.github.com>
2024-11-16 00:36:00 -0300

    Add files via upload
    Signed-off-by: Perro-Wacho <111267236+Perro-Wacho@users.noreply.github.com>

---
+++ utils/cambio_rol_usuario.php
@@ -0,0 +1,30 @@
+<?php
+    include '../database/conexion.php';
+
+    header('Content-Type: application/json');
+
+    $response = ["success" => false, "message" => ""];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $rut = $_POST['rut'];
+        $id_rol = $_POST['id_rol'];
+
+        $sql = "UPDATE usuario SET id_rol = ? WHERE rut = ?";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bind_param("is", $id_rol, $rut);
+
+        if ($stmt->execute()) {
+            $response["success"] = true;
+            $response["message"] = "Rol cambiado exitosamente.";
+        } else {
+            $response["message"] = "Error al cambiar el rol: " . $stmt->error;
+        }
+
+        $stmt->close();
+    } else {
+        $response["message"] = "Parámetros faltantes.";
+    }
+
+    $conexion->close();
+    echo json_encode($response);
+?>
