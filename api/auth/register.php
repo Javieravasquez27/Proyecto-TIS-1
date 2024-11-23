77b23afe5a47f5d9bf2bf7a56d6532c8c57e3134
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2024-11-23 16:43:59 -0300

    Validaciones formulario de registro
    Se implementa validación para evitar registros de RUT, nombres de usuario y correos ya existentes en la BD

--- api/auth/register.php
+++ api/auth/register.php
@@ -31,99 +31,131 @@ try {
         $rol = stripslashes($_REQUEST['rol']);
         $rol = mysqli_real_escape_string($conexion, $rol);
 
-        // Procesar la foto de perfil si el rol es Profesional (id_rol = 3)
-        $foto_perfil = null;
-        if ($rol == 3 && isset($_FILES['foto_perfil'])) {
-            $directorio_foto_perfil = "../../uploads/foto_perfil/";
-            $localizacion_foto_perfil = $directorio_foto_perfil . basename($_FILES["foto_perfil"]["name"]);
-            $subida_correcta_fp = 1;
-            $extension_fp = strtolower(pathinfo($localizacion_foto_perfil, PATHINFO_EXTENSION));
+        $sql_consulta_rut = "SELECT rut FROM usuario WHERE rut = '$rut';";
+        $resultado_consulta_rut = mysqli_query($conexion, $sql_consulta_rut);
 
-            // Verificar si el archivo es una imagen
-            $verificar_fp = getimagesize($_FILES["foto_perfil"]["tmp_name"]);
-            if($verificar_fp !== false) {
-                $subida_correcta_fp = 1;
-            } else {
-                $subida_correcta_fp = 0;
-            }
+        $sql_consulta_nombre_usuario = "SELECT nombre_usuario FROM usuario WHERE nombre_usuario = '$nombre_usuario';";
+        $resultado_consulta_nombre_usuario = mysqli_query($conexion, $sql_consulta_nombre_usuario);
 
-            // Intentar cargar el archivo
-            if ($subida_correcta_fp == 1 && move_uploaded_file($_FILES["foto_perfil"]["tmp_name"], $localizacion_foto_perfil)) {
-                $foto_perfil = mysqli_real_escape_string($conexion, $localizacion_foto_perfil);
-            } else {
-                $response = array(
-                    'success' => false,
-                    'message' => 'Error al cargar la foto de perfil.'
-                );
-                echo json_encode($response);
-                exit();
-            }
+        $sql_consulta_correo = "SELECT correo FROM usuario WHERE correo = '$correo';";
+        $resultado_consulta_correo = mysqli_query($conexion, $sql_consulta_correo);
+
+        $errores = [];
+
+        if ($resultado_consulta_rut->num_rows > 0) {
+            $errores[] = 'El RUT ingresado ya se encuentra registrado.';
         }
 
-        // Si el rol es profesional, insertar en la tabla profesional
-        if ($rol == 3) {
-            $sql_ingreso_usuario = "INSERT INTO usuario (rut, nombre_usuario, nombres, apellido_p, apellido_m, correo, telefono, contrasena, fecha_nac, direccion, foto_perfil, id_comuna, id_rol, id_estado_usuario) 
-                                    VALUES ('$rut', '$nombre_usuario', '$nombres', '$apellido_p', '$apellido_m', '$correo', '$telefono', '" . md5($password) . "', '$fecha_nac', '$direccion', '$foto_perfil', '$comuna', '$rol', 2)";
-            $resultado_usuario = mysqli_query($conexion, $sql_ingreso_usuario);
+        if ($resultado_consulta_nombre_usuario->num_rows > 0) {
+            $errores[] = 'El nombre de usuario ingresado ya se encuentra registrado.';
+        }
 
-            if ($resultado_usuario)
-            {
-                $id_profesion = stripslashes($_REQUEST['profesion']);
-                $id_profesion = mysqli_real_escape_string($conexion, $id_profesion);
-                $id_institucion = stripslashes($_REQUEST['institucion']);
-                $id_institucion = mysqli_real_escape_string($conexion, $id_institucion);
-                $experiencia = stripslashes($_REQUEST['experiencia']);
-                $experiencia = mysqli_real_escape_string($conexion, $experiencia);
-                
-                $titulo_profesional = null;
-                if (isset($_FILES['titulo_profesional'])) {
-                    $directorio_titulo_profesional = "../../uploads/titulo_profesional/";
-                    $localizacion_titulo_profesional = $directorio_titulo_profesional . basename($_FILES["titulo_profesional"]["name"]);
-                    $subida_correcta_tp = 1;
-                    $extension_tp = strtolower(pathinfo($localizacion_titulo_profesional, PATHINFO_EXTENSION));
-                
-                    // Intentar cargar el archivo
-                    if ($subida_correcta_tp == 1 && move_uploaded_file($_FILES["titulo_profesional"]["tmp_name"], $localizacion_titulo_profesional)) {
-                        $titulo_profesional = mysqli_real_escape_string($conexion, $localizacion_titulo_profesional);
-                    } else {
-                        $response = array(
-                            'success' => false,
-                            'message' => 'Error al cargar título profesional.'
-                        );
-                        echo json_encode($response);
-                        exit();
-                    }
+        if ($resultado_consulta_correo->num_rows > 0) {
+            $errores[] = 'El correo ingresado ya se encuentra registrado.';
+        }
+
+        if (!empty($errores)) {
+            $response = [
+                'success' => false,
+                'message' => implode(' ', $errores)
+            ];
+        }
+
+        else {
+            // Procesar la foto de perfil si el rol es Profesional (id_rol = 3)
+            $foto_perfil = null;
+            if ($rol == 3 && isset($_FILES['foto_perfil'])) {
+                $directorio_foto_perfil = "../../uploads/foto_perfil/";
+                $localizacion_foto_perfil = $directorio_foto_perfil . basename($_FILES["foto_perfil"]["name"]);
+                $subida_correcta_fp = 1;
+                $extension_fp = strtolower(pathinfo($localizacion_foto_perfil, PATHINFO_EXTENSION));
+
+                // Verificar si el archivo es una imagen
+                $verificar_fp = getimagesize($_FILES["foto_perfil"]["tmp_name"]);
+                if($verificar_fp !== false) {
+                    $subida_correcta_fp = 1;
+                } else {
+                    $subida_correcta_fp = 0;
                 }
 
-                $sql_ingreso_profesional = "INSERT INTO profesional (rut, id_profesion, id_institucion, experiencia, titulo_profesional) 
-                                            VALUES ('$rut', '$id_profesion', '$id_institucion', '$experiencia', '$titulo_profesional')";
-                $resultado_profesional = mysqli_query($conexion, $sql_ingreso_profesional);
+                // Intentar cargar el archivo
+                if ($subida_correcta_fp == 1 && move_uploaded_file($_FILES["foto_perfil"]["tmp_name"], $localizacion_foto_perfil)) {
+                    $foto_perfil = mysqli_real_escape_string($conexion, $localizacion_foto_perfil);
+                } else {
+                    $response = array(
+                        'success' => false,
+                        'message' => 'Error al cargar la foto de perfil.'
+                    );
+                    echo json_encode($response);
+                    exit();
+                }
             }
-            $message = 'Hemos recibido su solicitud de registro como profesional y será revisada.';
-        }
 
-        elseif ($rol == 4)
-        {
-            $sql_ingreso_usuario = "INSERT INTO usuario (rut, nombre_usuario, nombres, apellido_p, apellido_m, correo, telefono, contrasena, fecha_nac, direccion, foto_perfil, id_comuna, id_rol, id_estado_usuario) 
-                                    VALUES ('$rut', '$nombre_usuario', '$nombres', '$apellido_p', '$apellido_m', '$correo', '$telefono', '" . md5($password) . "', '$fecha_nac', '$direccion', '$foto_perfil', '$comuna', '$rol', 1)";
-            $resultado_usuario = mysqli_query($conexion, $sql_ingreso_usuario);
+            // Si el rol es profesional, insertar en la tabla profesional
+            if ($rol == 3) {
+                $sql_ingreso_usuario = "INSERT INTO usuario (rut, nombre_usuario, nombres, apellido_p, apellido_m, correo, telefono, contrasena, fecha_nac, direccion, foto_perfil, id_comuna, id_rol, id_estado_usuario) 
+                                        VALUES ('$rut', '$nombre_usuario', '$nombres', '$apellido_p', '$apellido_m', '$correo', '$telefono', '" . md5($password) . "', '$fecha_nac', '$direccion', '$foto_perfil', '$comuna', '$rol', 2)";
+                $resultado_usuario = mysqli_query($conexion, $sql_ingreso_usuario);
 
-            if ($resultado_usuario)
+                if ($resultado_usuario)
+                {
+                    $id_profesion = stripslashes($_REQUEST['profesion']);
+                    $id_profesion = mysqli_real_escape_string($conexion, $id_profesion);
+                    $id_institucion = stripslashes($_REQUEST['institucion']);
+                    $id_institucion = mysqli_real_escape_string($conexion, $id_institucion);
+                    $experiencia = stripslashes($_REQUEST['experiencia']);
+                    $experiencia = mysqli_real_escape_string($conexion, $experiencia);
+
+                    $titulo_profesional = null;
+                    if (isset($_FILES['titulo_profesional'])) {
+                        $directorio_titulo_profesional = "../../uploads/titulo_profesional/";
+                        $localizacion_titulo_profesional = $directorio_titulo_profesional . basename($_FILES["titulo_profesional"]["name"]);
+                        $subida_correcta_tp = 1;
+                        $extension_tp = strtolower(pathinfo($localizacion_titulo_profesional, PATHINFO_EXTENSION));
+                    
+                        // Intentar cargar el archivo
+                        if ($subida_correcta_tp == 1 && move_uploaded_file($_FILES["titulo_profesional"]["tmp_name"], $localizacion_titulo_profesional)) {
+                            $titulo_profesional = mysqli_real_escape_string($conexion, $localizacion_titulo_profesional);
+                        } else {
+                            $response = array(
+                                'success' => false,
+                                'message' => 'Error al cargar título profesional.'
+                            );
+                            echo json_encode($response);
+                            exit();
+                        }
+                    }
+
+                    $sql_ingreso_profesional = "INSERT INTO profesional (rut, id_profesion, id_institucion, experiencia, titulo_profesional) 
+                                                VALUES ('$rut', '$id_profesion', '$id_institucion', '$experiencia', '$titulo_profesional')";
+                    $resultado_profesional = mysqli_query($conexion, $sql_ingreso_profesional);
+                }
+                $message = 'Hemos recibido su solicitud de registro como profesional y será revisada.';
+            }
+
+            elseif ($rol == 4)
             {
-                $sql_ingreso_cliente = "INSERT INTO cliente (rut) VALUES ('$rut')";
-                $resultado_cliente = mysqli_query($conexion, $sql_ingreso_cliente);
-            }
-            $message = 'Registro exitoso';
-        }
+                $sql_ingreso_usuario = "INSERT INTO usuario (rut, nombre_usuario, nombres, apellido_p, apellido_m, correo, telefono, contrasena, fecha_nac, direccion, foto_perfil, id_comuna, id_rol, id_estado_usuario) 
+                                        VALUES ('$rut', '$nombre_usuario', '$nombres', '$apellido_p', '$apellido_m', '$correo', '$telefono', '" . md5($password) . "', '$fecha_nac', '$direccion', '$foto_perfil', '$comuna', '$rol', 1)";
+                $resultado_usuario = mysqli_query($conexion, $sql_ingreso_usuario);
 
-        $response = array(
-            'success' => true,
-            'message' => $message
-        );
+                if ($resultado_usuario)
+                {
+                    $sql_ingreso_cliente = "INSERT INTO cliente (rut) VALUES ('$rut')";
+                    $resultado_cliente = mysqli_query($conexion, $sql_ingreso_cliente);
+                }
+                $message = 'Registro exitoso';
+            }
+
+            $response = array(
+                'success' => true,
+                'message' => $message
+            );
+        }
     } else {
         $response = array(
             'success' => false,
-            'message' => 'Error en los datos recibidos'
+            'message' => 'Error en los datos recibidos.'
         );
     }
     
