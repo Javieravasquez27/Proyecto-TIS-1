fb4f805e2c73d8ec145d3ad5f37fe5074e2119c6
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2024-11-23 23:34:31 -0300

    Se agregan páginas a navbar de usuario no logueado

--- includes/navbar.php
+++ includes/navbar.php
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 <body style="background-color: rgb(240, 223, 255);">
     <nav class="navbar sticky-top navbar-expand-lg bg-gradient bg-opacity-50" style="background-color: rgb(150, 120, 182);">
         <div class="container-fluid ">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="index.php?p=home">
                 <img src="public/images/logo.png" alt="Logo KindomJobs" height="50" class="imagen d-inline-block">
                 <span class="h3 align-middle" style="color: black;">KindomJob's</span>
             </a>
@@ -56,6 +56,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($pagina == 'home') ? 'active' : null ?>" aria-current="page" href="index.php?p=home">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo ($pagina == 'pages/profesiones') ? 'active' : null ?>" aria-current="page" href="#">Profesiones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo ($pagina == 'pages/servicios') ? 'active' : null ?>" aria-current="page" href="#">Servicios</a>
                     </li>
                 </ul>
                 <div class="d-flex">
