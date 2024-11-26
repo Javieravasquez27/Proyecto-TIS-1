03cb0ecd5c85ec66b4a6d1097e86716adf09d057
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2024-11-26 16:18:52 -0300

    Se simplifican consultas para evitar errores

--- pages/profesional/profile_cita.php
+++ pages/profesional/profile_cita.php
@@ -3,36 +3,47 @@
     include("middleware/auth.php");
     include("database/conexion.php");
     $rut = $_GET['rut'];
+
+    $consulta_profesional = "SELECT * FROM comuna, institucion, usuario JOIN profesional USING (rut)
+                                           JOIN profesion USING (id_profesion)
+                             WHERE profesional.id_institucion = institucion.id_institucion
+                             AND usuario.id_comuna = comuna.id_comuna
+                             AND profesional.rut = '$rut';";
+    $resultado_profesional = mysqli_query($conexion, $consulta_profesional);
+    $fila_profesional = mysqli_fetch_assoc($resultado_profesional);
+
+    $consulta_serv_prof = "SELECT sp.id_servicio, sp.rut_profesional, s.nombre_servicio AS nombre_servicio,
+                                  sp.precio_serv_prof
+                           FROM servicio_profesional sp LEFT JOIN servicio s ON sp.id_servicio = s.id_servicio 
+                           WHERE rut_profesional = '$rut';";
+    $resultado_serv_prof = mysqli_query($conexion, $consulta_serv_prof);
+
+    $consulta_lugar_at_presencial = "SELECT nombre_comuna, nombre_provincia, nombre_region
+                                     FROM lugar_atencion_presencial JOIN comuna USING (id_comuna)
+                                          JOIN provincia USING (id_provincia) JOIN region USING (id_region)
+                                     WHERE rut_profesional = '$rut';";
+    $resultado_lugar_at_presencial = mysqli_query($conexion, $consulta_lugar_at_presencial);
 ?>
 
 <link rel="stylesheet" href="public/css/profile_cita.css">
 
 <div class="container my-5">
-    <?php
-    $query = "SELECT * FROM comuna, institucion, usuario join profesional using (rut) join profesion using (id_profesion)
-            where profesional.id_institucion = institucion.id_institucion
-            and usuario.id_comuna = comuna.id_comuna
-            and profesional.rut = '$rut'";
-    $resultado = mysqli_query($conexion,$query);
-    $row_prof = mysqli_fetch_assoc($resultado);
-    ?>
-
     <title>Reservar cita profesional con
-        <?php echo $row_prof['nombres']; ?> - KindomJob's
+        <?php echo $fila_profesional['nombres']; ?> - KindomJob's
     </title>
 
     <div class="profile-header">
-        <img src="<?php  echo $row_prof['foto_perfil'] ?>" alt="Foto de perfil" class="rounded-circle mb-3">
+        <img src="<?php echo $fila_profesional['foto_perfil'] ?>" alt="Foto de perfil" class="rounded-circle mb-3">
         <h2>
-            <?php echo $row_prof['nombres']?>
-            <?php echo $row_prof['apellido_p']?>
-            <?php echo $row_prof['apellido_m']?>
+            <?php echo $fila_profesional['nombres']?>
+            <?php echo $fila_profesional['apellido_p']?>
+            <?php echo $fila_profesional['apellido_m']?>
         </h2>
         <p>
-            <?php echo $row_prof['nombre_profesion']?>
+            <?php echo $fila_profesional['nombre_profesion']?>
         </p>
         <p>
-            <?php echo $row_prof['nombre_comuna']?>
+            <i class="bi bi-geo-alt"></i> <?php echo $fila_profesional['nombre_comuna']?>
         </p>
     </div>
     <div class="row mt-4">
@@ -60,41 +71,37 @@
                 <div class="tab-pane fade show active" id="services" role="tabpanel">
                     <h5>Servicios y Precios</h5>
                     <?php
-                    $query = "SELECT nombre_servicio, precio_serv_prof from servicio_profesional join servicio using (id_servicio)
-                    where rut_profesional = '$rut'";
-                    $resultado_prof = mysqli_query($conexion, $query);
-                    while($row_prof = mysqli_fetch_assoc($resultado_prof)){
+                        while($fila_serv_prof = mysqli_fetch_assoc($resultado_serv_prof)){
                     ?>
                     <div class="service-item"><span>
-                            <?php echo $row_prof['nombre_servicio'] ?>
+                            <?php echo $fila_serv_prof['nombre_servicio'] ?>
                         </span><span>$
-                            <?php echo $row_prof['precio_serv_prof'] ?>
+                            <?php echo $fila_serv_prof['precio_serv_prof'] ?>
                         </span></div>
                     <?php
-                    }
+                        }
                     ?>
                     <div class="graph-placeholder">[Gráfico]</div>
                 </div>
                 <div class="tab-pane fade" id="direcciones" role="tabpanel">
                     <h5>Direcciones</h5>
                     <?php
-                    $query = "SELECT nombre_comuna, nombre_provincia,nombre_region from lugar_atencion_presencial join comuna using (id_comuna) join provincia using (id_provincia) join region using (id_region)
-                    where rut_profesional = '$rut'";
-                    $resultado_prof = mysqli_query($conexion, $query);
-                    while($row_prof = mysqli_fetch_assoc($resultado_prof)){
+                        while($fila_lugar_at_presencial = mysqli_fetch_assoc($resultado_lugar_at_presencial)){
                     ?>
                     <div class="service-item"><span><b>Direccion: </b>
-                            <?php echo $row_prof['nombre_comuna'] ?> ,
-                            <?php echo $row_prof['nombre_provincia'] ?> ,
-                            <?php echo $row_prof['nombre_region'] ?>
+                            <?php echo $fila_lugar_at_presencial['nombre_comuna'] ?>,
+                            <?php echo $fila_lugar_at_presencial['nombre_provincia'] ?>,
+                            <?php echo $fila_lugar_at_presencial['nombre_region'] ?>
                     </div>
                     <?php
-                    }
+                        }
                     ?>
                 </div>
                 <div class="tab-pane fade" id="experience" role="tabpanel">
                     <h5>Experiencia</h5>
-                    <p>esto lo escribe el profesional</p>
+                    <p>
+                        <?php echo $fila_profesional['experiencia'] ?>
+                    </p>
                 </div>
                 <div class="tab-pane fade" id="opinions" role="tabpanel">
                     <h5>Opiniones</h5>
@@ -110,16 +117,17 @@
                 <h5 class="mb-3">Reservar Cita</h5>
                 <input type="hidden" id="rut_prof" value="<?php echo $rut?>">
                 <select class="form-select mb-3">
-                    <option value="" selected>Servicio</option>
+                    <option value="" selected>Seleccione un servicio</option>
                     <?php
-                                $nombre_servicio="SELECT * FROM servicio";
-                                $resultado_servicio=mysqli_query($conexion,$nombre_servicio);
-                                while($row_servicio= mysqli_fetch_assoc($resultado_servicio)){
-                                    $nombre = $row_servicio["nombre_servicio"];
-                                    $id = $row_servicio["id_servicio"];
-                                    echo "<option value=".$id.">".$nombre."</option>";
-                                }
-                            ?>
+                        $consulta_select_sp = $consulta_serv_prof;
+                        $resultado_select_sp = mysqli_query($conexion, $consulta_select_sp);
+
+                        while($fila_select_sp = mysqli_fetch_assoc($resultado_select_sp)){
+                            $nombre = $fila_select_sp['nombre_servicio'];
+                            $id = $fila_select_sp['id_servicio'];
+                            echo "<option value=".$id.">".$nombre."</option>";
+                        }
+                    ?>
                 </select>
                 <input type="date" class="form-control mb-3" id="fecha" name="fecha">
                 <div class="d-grid gap-2" id="horas-disponibles">
@@ -139,7 +147,7 @@
                 type: 'POST',
                 data: { rut: rut, fecha: fechaSeleccionada },
                 success: function (data) {
-                    // Actualiza la lista de horas disponibles
+                    // Se actualiza la lista de horas disponibles
                     $('#horas-disponibles').html(data);
                 },
                 error: function () {
