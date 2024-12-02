ef7e102baafc5057f406c0ebee0477d4acf7d3ab
Neovix33 <112022312+Neovix33@users.noreply.github.com>
2024-12-02 00:22:40 -0300

    elimina todo por error

--- pages/error/acceso_denegado.php
+++
@@ -1,7 +0,0 @@
-<title>Acceso Denegado - KindomJob's</title>
-
-<div class="container text-center">
-    <h1 class="text-center my-5">Acceso Denegado</h1>
-    <p class="text-center">No tienes permiso para acceder a esta página.</p>
-    <a class="btn btn-primary" onclick="history.back();">Regresar</a>
-</div>
